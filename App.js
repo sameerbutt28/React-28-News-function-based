48018b9d7b214798940f9d2f15d20cdeff3321d7
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-09-21 23:59:34 +0500

    Add files via upload

--- App.js
+++ App.js
@@ -13,7 +13,17 @@ const App =()=>
       <Main>
         <Navbar/>
       <Routes>
-<Route exact path='/news' element={<News pageSize={8}  category="general"/>}/>
+<Route exact path='/news' element={<News pageSize={8}  category="all"/>}/>
+<Route exact path='/business' element={<News pageSize={8}  category="business"/>}/>
+<Route exact path='/business' element={<News pageSize={8}  category="business"/>}/>
+<Route exact path='/entertainment' element={<News pageSize={8}  category="entertainment"/>}/>
+<Route exact path='/general' element={<News pageSize={8}  category="general"/>}/>
+<Route exact path='/health' element={<News pageSize={8}  category="health"/>}/>
+<Route exact path='/science' element={<News pageSize={8}  category="science"/>}/>
+<Route exact path='/sports' element={<News pageSize={8}  category="sports"/>}/>
+<Route exact path='/business' element={<News pageSize={8}  category="business"/>}/>
+<Route exact path='/technology' element={<News pageSize={8}  category="technology"/>}/>
+
 
 
 <Route exact path='/about' element={<About/>}/>
